429bb62596b27b027b925dffd02e5d9961eb2bd5
Tupalski Ilya <itupalski@gmail.com>
2023-12-28 11:06:47 +0400

    feat: styles filter block

--- src/magento/module-catalog/components/Filters/Filters.tsx
+++ src/magento/module-catalog/components/Filters/Filters.tsx
@@ -3,6 +3,9 @@
 import { useState } from "react";
 import PriceFilter from "../PriceFilter/PriceFilter";
 
+import trash from "@/assets/icons/heroiconsTrash.svg";
+import Image from "next/image";
+
 export default function Filters() {
     const [price, setPrice] = useState([135, 1256]);
 
@@ -17,17 +20,24 @@ export default function Filters() {
     };
 
     return (
-        <div className="w-1/4 bg-white rounded-xl p-3 font-monts mb-6">
+        <div className="w-1/4 bg-white flex flex-col gap-3 rounded-xl p-3 font-monts mb-6">
+            <div className="flex justify-between items-center">
+                <span className="font-semibold">Filters</span>
+                <button className="text-xs font-semibold text-red-400 flex items-end">
+                    <Image src={trash} width={17} alt="Clear" />
+                    Clear
+                </button>
+            </div>
+            <hr />
             <PriceFilter
                 value={price}
                 setPriceSlider={handleChangePriceSlider}
                 setPriceInput={handleChangePriceInput}
             />
-            <div className="py-5">
-                <button className="bg-gr-green w-full h-9 rounded-md text-white font-monts">
-                    Search
-                </button>
-            </div>
+            <hr />
+            <button className="bg-gr-green w-full h-9 rounded-md text-white font-monts">
+                Search
+            </button>
         </div>
     );
 }
